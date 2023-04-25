4a1b9fe8581a595f5ed0899145342f693b8dcfcf
GrayDiamond493 <47148399+GrayDiamond493@users.noreply.github.com>
2023-04-25 18:23:17 -0500

    test routes

--- test/routes-test.spec.ts
+++ test/routes-test.spec.ts
@@ -1,9 +1,36 @@
 import { assert } from "chai";
-import { router } from "../src/routes";
+import request from 'supertest';
+import express from 'express';
+import { router } from '../src/routes';
 
-describe("Router Tests", () => {
-    it("Test", function () {
-      
-    });
+describe('router', () => {
+  const app = express();
+  app.use('/', router);
 
+  it('Test GET /', async () => {
+    const response = await request(app).get('/');
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.text, 'Bienvenido! Esta aplicación ayuda a elevar números a sus potencias y a entender AWS.');
   });
+
+  it('Test Hola', async () => {
+    const response = await request(app).get('/hola');
+    assert.strictEqual(response.status, 200);
+    assert.equal(response.text, 'Hola Mundo');
+  });
+
+  it('Test Square', async () => {
+    const response = await request(app).get('/cuadrado');
+    assert.strictEqual(response.status, 200);
+  });
+
+  it('Test Cube', async () => {
+    const response = await request(app).get('/cubo');
+    assert.strictEqual(response.status, 200);
+  });
+});
+
+
+
+
+
