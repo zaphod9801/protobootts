5b2a56f9bbffa7ab70f23d21ed91c63bbb1d4cba
zaphod9801 <johanruiz932@gmail.com>
2023-04-27 11:37:31 -0500

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb', 'airbnb-typescript', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
   parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'no-console': 'warn',
-  },
-  overrides: [
-    {
-      files: ['**/*.json'],
-      rules: {
-        semi: ['off'],
-        quotes: ['off'],
-      },
-    },
-    {
-      files: ['**/*.js', '**/*.ts'],
-      rules: {
-        semi: ['error', 'always'],
-      }
-    },
+  plugins: [
+    '@typescript-eslint',
+    'import', // Add this line to include the import plugin
   ],
+  rules: {
+    // Add or update your rules here
+  },
 };
